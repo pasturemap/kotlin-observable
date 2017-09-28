16fc2ee367c9926af2c2486b3d69277e88675da3
iverc <ivan@pasturemap.com>
2017-09-28 21:27:49 +0300

    Modify ObservableProperty

--- src/main/java/com/lightningkite/kotlin/observable/property/ObservableProperty.kt
+++ src/main/java/com/lightningkite/kotlin/observable/property/ObservableProperty.kt
@@ -11,7 +11,7 @@ interface ObservableProperty<T> : MutableCollection<(T) -> Unit> {
     /**
      * The current value of the observable property.
      */
-    val value: T
+    var value: T
 
     operator fun getValue(thisRef: Any?, prop: KProperty<*>): T {
         return value
